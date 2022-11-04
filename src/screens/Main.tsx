411fad8955430dc5681e844df554507bf6241589
barash777 <barash.vertihvost@gmail.com>
2022-11-04 19:30:32 +0300

    feat: add screens

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import {Text, View} from "react-native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {RootStackParamList} from "./types";
+import {HomeScreen} from "./Home/HomeScreen";
+import {DetailsScreen} from "./Details/DetailsScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Main = () => {
     return (
-        <View>
-            <Text>Main</Text>
+        <View style={{flex: 1}}>
+            <Stack.Navigator>
+                <Stack.Screen name={'Home'} component={HomeScreen}/>
+                <Stack.Screen name={'Details'} component={DetailsScreen}/>
+            </Stack.Navigator>
         </View>
     );
 };

53d985e6a9ab3ff5b8a489fa3621e787a0c3abfe
barash777 <barash.vertihvost@gmail.com>
2022-11-04 19:50:07 +0300

    feat: get all pokemons in Home

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from "react-native";
+import {api, PokemonItem} from "../../api/api";
 
 export const HomeScreen = () => {
+    const [allPokemons, setAllPokemons] = useState<PokemonItem[]>([]);
+
+    useEffect(() => {
+        api.getAllPokemons()
+            .then(res => {
+                setAllPokemons(res.data.results)
+            })
+    }, []);
+
     return (
         <View>
             <Text>Home</Text>
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,23 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {View} from "react-native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {RootStackParamList} from "./types";
 import {HomeScreen} from "./Home/HomeScreen";
 import {DetailsScreen} from "./Details/DetailsScreen";
-import {api, PokemonItem} from "../api/api";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Main = () => {
-    const [allPokemons, setAllPokemons] = useState<PokemonItem[]>([]);
-
-    useEffect(() => {
-        api.getAllPokemons()
-            .then(res => {
-                setAllPokemons(res.data.results)
-            })
-    }, []);
-
     return (
         <View style={{flex: 1}}>
             <Stack.Navigator>

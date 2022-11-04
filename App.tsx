0e0a821d105b17fe1e6f85e0c0cb202751993e48
barash777 <barash.vertihvost@gmail.com>
2022-11-04 19:15:11 +0300

    feat: create screens and install navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {Main} from "./src/screens/Main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <View style={styles.container}>
+              <Main/>
+          </View>
+      </NavigationContainer>
   );
 }
 
@@ -14,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });

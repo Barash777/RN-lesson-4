62b12c81513b70209e59ff593aee5393f45a9dd2
barash777 <barash.vertihvost@gmail.com>
2022-11-04 19:29:19 +0300

    feat: add types for Stack

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Text, View} from "react-native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {RootStackParamList} from "./types";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Main = () => {
     return (

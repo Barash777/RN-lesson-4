a7f0a764d01efa680097a0cfb98cdaccf7f8fc23
barash777 <barash.vertihvost@gmail.com>
2022-11-04 21:23:56 +0300

    feat: dispatch thunk in DetailsScreen

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Image, Text, View} from "react-native";
 import {DetailsPropsType} from "../types";
-import {api, Pokemon} from "../../api/api";
+import {useAppDispatch, useAppSelector} from "../../store/store";
+import {getPokemonById} from "../../store/rootSlice";
 
 export const DetailsScreen = (props: DetailsPropsType) => {
-    const [pokemon, setPokemon] = useState<Pokemon | null>();
+    // const [pokemon, setPokemon] = useState<Pokemon | null>();
+    const pokemon = useAppSelector(state => state.root.pokemon)
+    const dispatch = useAppDispatch()
 
     useEffect(() => {
-        api.getPokemonById(props.route.params.url)
-            .then(res => {
-                setPokemon(res.data)
-            })
+        // api.getPokemonById(props.route.params.url)
+        //     .then(res => {
+        //         setPokemon(res.data)
+        //     })
+        dispatch(getPokemonById(props.route.params.url))
     }, []);
 
     return (

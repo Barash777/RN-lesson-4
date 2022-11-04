73579f80ef522f5f4678426957493cadcbb4c2b6
barash777 <barash.vertihvost@gmail.com>
2022-11-04 19:59:58 +0300

    feat: add types for RootStackParamsList

--- src/screens/types.ts
+++ src/screens/types.ts
@@ -1,4 +1,14 @@
-export type RootStackParamList = {
+import {NavigationProp, useNavigation} from "@react-navigation/native";
+import {NativeStackScreenProps} from "@react-navigation/native-stack";
+
+export type RootStackParamsList = {
     Home: undefined
-    Details: undefined
+    Details: {
+        url: string
+    }
 }
+
+export type DetailsPropsType = NativeStackScreenProps<RootStackParamsList, 'Details'>
+export type NavigationUseType = NavigationProp<RootStackParamsList>
+
+export const useAppNavigation = () => useNavigation<NavigationUseType>()
